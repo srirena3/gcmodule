d9386c7751e59e8cd7247b0559847fb6cf539194
srirena3 <srinivas.shamkura@sada.com>
2022-07-07 23:59:53 -0500

    prj-module moveout of projects folder

--- projects/main.tf
+++ projects/main.tf
@@ -1,6 +1,6 @@
 module "shared_vpc_host_project" {
-  source          = "../modules/projects"
-  name            = "prj-${local.environment}-${local.svpc_project_label}"
+  source          = "../prjmodule"
+  name            = var.prjname
   project_id      = "prj-${local.environment}-${local.svpc_project_label}"
   services        = local.svpc_service_apis
   billing_account = var.billing_account
